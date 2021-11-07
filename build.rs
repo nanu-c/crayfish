60993b8682cd0233f0082b19df5420f7e4735897
aaron@nanu-c.org <aaron@nan-c.org>
2021-11-07 21:02:42 +0100

    initial commit

---
+++ build.rs
@@ -0,0 +1,67 @@
+/* Copyright (C) 2018 Olivier Goffart <ogoffart@woboq.com>
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+use std::path::Path;
+
+use failure::*;
+
+fn protobuf() -> Result<(), Error> {
+    let protobuf = Path::new("protobuf").to_owned();
+
+    let input: Vec<_> = protobuf
+        .read_dir()
+        .expect("protobuf directory")
+        .filter_map(|entry| {
+            let entry = entry.expect("readable protobuf directory");
+            let path = entry.path();
+            if Some("proto") == path.extension().and_then(std::ffi::OsStr::to_str) {
+                assert!(path.is_file());
+                println!("cargo:rerun-if-changed={}", path.to_str().unwrap());
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    prost_build::compile_protos(&input, &[protobuf])?;
+    Ok(())
+}
+
+
+fn main() {
+    protobuf().unwrap();
+
+    // Print a warning when rustc is too old.
+    if !version_check::is_min_version("1.48.0").unwrap_or(false) {
+        if let Some(version) = version_check::Version::read() {
+            panic!(
+                "Crayfish requires Rust 1.48.0 or later.  You are using rustc {}",
+                version
+            );
+        } else {
+            panic!(
+                "Crayfish requires Rust 1.48.0 or later, but could not determine Rust version.",
+            );
+        }
+    }
+    let mer_target_root = "";
+
+    let mut cfg = cpp_build::Config::new();
+
+    cfg.flag(&format!("--sysroot={}", mer_target_root));
+    cfg.flag("-isysroot");
+    cfg.flag(&mer_target_root);
+    cfg.build("src/main.rs");
+}
---
+++ src/signal_service/sealedsender.rs
@@ -0,0 +1,84 @@
+use crate::config::SignalConfig;
+use crate::error::Result;
+use crate::store::Storage;
+use crate::store::StorageLocation;
+extern crate base64;
+
+use libsignal_service::ServiceAddress;
+use libsignal_service::cipher::ServiceCipher;
+use libsignal_service::configuration::ServiceConfiguration;
+use libsignal_service::configuration::SignalServers;
+use libsignal_service::prelude::Envelope;
+
+use prost::Message;
+// use protocol::envelope::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Debug)]
+pub struct SealedSenderMessage {
+    // pub uuid: String,
+    #[serde(with = "serde_str")]
+    pub message: String,
+}
+
+#[derive(Serialize)]
+pub struct DecryptSealedMessageResponse {
+    // its base64 encoded libsignal_service::prelude::Content
+    pub message: String,
+    pub sender: ServiceAddress,
+    pub sender_device: u32,
+    pub timestamp: u64,
+    pub needs_receipt: bool,
+}
+fn service_cfg() -> ServiceConfiguration {
+    // XXX: read the configuration files!
+    SignalServers::Production.into()
+}
+pub async fn decrypt_sealed_message(
+    data: SealedSenderMessage,
+) -> Result<DecryptSealedMessageResponse> {
+    println!("decrypt_sealed_message");
+
+    let service_cfg = service_cfg();
+    let config = SignalConfig::default();
+    let storage = open_storage(&config).await?;
+    let signaling_key = storage.signaling_key().await?;
+    println!("signaling_key loaded");
+    let mut cipher = ServiceCipher::new(
+        storage.clone(),
+        storage.clone(),
+        storage.clone(),
+        storage.clone(),
+        rand::thread_rng(),
+        service_cfg.credentials_validator().expect("trust root"),
+    );
+    println!("cipher created");
+    // let msg = data.message.into_bytes();
+    let msg = base64::decode(data.message).unwrap();
+    let envelope = Envelope::decrypt(&msg, &signaling_key, false)?;
+    let content = cipher.open_envelope(envelope).await?.unwrap();
+    println!("content decrypted {:?}", content.body);
+    let content_vec = content.body.into_proto().encode_to_vec();
+    let message = base64::encode(&content_vec);
+    Ok(DecryptSealedMessageResponse {
+        message: message,
+        sender_device: content.metadata.sender_device,
+        timestamp: content.metadata.timestamp,
+        needs_receipt: content.metadata.needs_receipt,
+        sender: content.metadata.sender,
+    })
+}
+
+async fn open_storage(config: &crate::config::SignalConfig) -> anyhow::Result<Storage> {
+    let home = dirs::home_dir()
+        .unwrap()
+        .join(".local")
+        .join("share")
+        .join("textsecure.nanuc");
+    let location = StorageLocation::Path(home);
+    println!("opening storage at location {:?}", location.to_str());
+
+    let storage = Storage::open(&location).await?;
+    println!("opening storage finished {:?}", location.to_str());
+    Ok(storage)
+}
